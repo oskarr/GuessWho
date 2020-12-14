9ddb933e881c717704b14e5f426cb54eac4f918e
oskarr <oskarrune@gmail.com>
2020-12-14 13:52:23 +0100

    Fixed path issues

--- backend/constants.py
+++ backend/constants.py
@@ -1,14 +1,9 @@
-import gzip, os
+import gzip, os, pathlib, sys
+
+BASE_PATH = str(pathlib.Path(sys.argv[0]).resolve().parent.parent)
 
 FACES = ["static/NVSG/"+str(i)+".jpg" for i in range(74074001, 74075000, 2)]
 
 
-if os.path.isfile('names.gz'):
-    with gzip.open("names.gz", "r") as f:
-        NAMES = f.read().decode('utf8').strip().split("\n")
-elif os.path.isfile('backend/names.gz'): # Heroku needs this
-    with gzip.open("backend/names.gz", "r") as f:
-            NAMES = f.read().decode('utf8').strip().split("\n")
-else:
-    print("names.gz not found. Current directory contents: ", os.listdir())
-    raise FileNotFoundError
+with gzip.open(BASE_PATH + "/backend/names.gz", "r") as f:
+    NAMES = f.read().decode('utf8').strip().split("\n")
--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python3
 from aiohttp import web
-import sockethandler, os
+import sockethandler, os, sys, pathlib
 from sockethandler import organizer
+from constants import BASE_PATH
 
 async def index(request: web.Request):
     """Serve the client-side application."""
-    with open('../frontend/index.html') as f:
+    with open(sys.argv[0] + '/../frontend/index.html') as f:
         return web.Response(text=f.read(), content_type='text/html')
 
 async def room(request: web.Request):
@@ -13,7 +14,7 @@ async def room(request: web.Request):
     room = organizer.getRoomById(roomid)
     if not room:
         return web.HTTPFound(location="/?err=roomnotfound")
-    with open('../frontend/room.html') as f:
+    with open(sys.argv[0] + '/../frontend/room.html') as f:
         return web.Response(text=f.read(), content_type='text/html')
 
 async def newroom(request: web.Request):
@@ -23,18 +24,12 @@ async def newroom(request: web.Request):
 
 
 def main(host, port):
+    print("[INFO] BASE_PATH =", BASE_PATH)
     app = web.Application()
     sockethandler.attach(app)
 
-    if os.path.isdir('frontend'):
-        app.router.add_static('/static', 'frontend/static')
-        app.router.add_static('/app', 'frontend/app')
-    elif os.path.isdir('../frontend'):
-        app.router.add_static('/static', '../frontend/static')
-        app.router.add_static('/app', '../frontend/app')
-    else:
-        print("/frontend not found. Current directory contents: ", os.listdir())
-        raise FileNotFoundError
+    app.router.add_static('/static', BASE_PATH + '/frontend/static')
+    app.router.add_static('/app', BASE_PATH + '/frontend/app')
 
     app.router.add_get('/', index)
     app.router.add_get('/room/{roomid}', room)
@@ -43,5 +38,5 @@ def main(host, port):
     
 
 if __name__ == '__main__':
-    main(host = "localhost", port = os.getenv('PORT'))
-    #main(host = "localhost", port = 8080)
+    #main(host = "localhost", port = os.getenv('PORT'))
+    main(host = "localhost", port = 8080)
